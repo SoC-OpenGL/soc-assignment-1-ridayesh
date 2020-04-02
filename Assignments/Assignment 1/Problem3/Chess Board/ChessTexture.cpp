8c6ce7af9169eb9cb63217d64d2f929a6e834050
ridayesh <61801122+ridayesh@users.noreply.github.com>
2020-04-02 19:45:10 +0530

    Update ChessTexture.cpp

--- Assignments/Assignment 1/Problem3/Chess Board/ChessTexture.cpp
+++ Assignments/Assignment 1/Problem3/Chess Board/ChessTexture.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
@@ -91,5 +91,5 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-} */
+} 
 
