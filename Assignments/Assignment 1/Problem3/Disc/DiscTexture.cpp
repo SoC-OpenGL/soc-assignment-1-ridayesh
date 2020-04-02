35ad17cb9b53e921521888fee60dcc8d6ec9b354
ridayesh <61801122+ridayesh@users.noreply.github.com>
2020-04-02 19:46:00 +0530

    Update DiscTexture.cpp

--- Assignments/Assignment 1/Problem3/Disc/DiscTexture.cpp
+++ Assignments/Assignment 1/Problem3/Disc/DiscTexture.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <glad/glad.h>
-/* #include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <cmath>
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -85,5 +85,5 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-} */
+} 
 
